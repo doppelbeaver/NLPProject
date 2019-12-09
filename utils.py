43871a01c313dc9eb55fa073a60ad0cdaa3d7ed6
Shailesh Mani Pandey <manipandey.shailesh@gmail.com>
2019-12-08 21:26:51 -0600

    Add a few utility functions

--- utils.py
+++ utils.py
@@ -1,5 +1,6 @@
 import csv
 import sys
+import random
 
 def read_tsv(input_file, quotechar=None):
     """Reads a tab separated value file."""
@@ -11,3 +12,25 @@ def read_tsv(input_file, quotechar=None):
                 line = list(unicode(cell, 'utf-8') for cell in line)
             lines.append(line)
         return lines
+
+def mark_random(input_file, output_file, num=50):
+    # Skip the labels
+    lines = read_tsv(input_file)[1:]
+    size = len(lines)
+    rset = set(random.sample(range(size), num))
+    with open(output_file, "w") as of:
+        for i, line in enumerate(lines):
+            oline = ''
+            if i in rset:
+                oline = 'r'
+            oline += '\t'.join(line) + '\n'
+            of.write(oline)
+
+def generate_mlm_data(input_file, output_file):
+    # Skip the labels
+    lines = read_tsv(input_file)[1:]
+    with open(output_file, "w") as of:
+        for line in lines:
+            of.write(line[3] + "\n")
+            of.write(line[4] + "\n")
+
